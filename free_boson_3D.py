601e0a5c900dd08de04b3e0d09faf0d2933043e2
lhayward <l3haywar@uwaterloo.ca>
2015-06-16 17:08:07 -0700

    Started to modify Sharmistha's code to work for 3D corners

---
+++ free_boson_3D.py
@@ -0,0 +1,78 @@
+import numpy as np
+from scipy import linalg
+import time
+import dbm
+import math
+import glob
+start_time = time.time()
+import os
+
+def getCornerEnt(Lx,Ly,Lz):
+  perx = pery = perz = False  #PBC or not along the x, y and z directions
+  massterm = 1
+
+  Ns = Lx * Ly * Lz
+
+  def site(x, y, z):
+    return x + (y*Lx) + (z*Lx*Ly) #convert (x,y,z) pair to a single site number
+
+  K = np.zeros((Ns, Ns))
+  #Loop to calculate the matrix K:
+  for x in range(Lx):
+    for y in range(Ly):
+      for z in range(Lz):
+        K[site(x,y,z),site(x,y,z)]= 6.0 + (massterm ** (2))
+        xp = (x+1)%Lx
+        yp = (y+1)%Ly
+        zp = (z+1)%Lz
+        if (xp > x) or perx:
+          K[site(xp,y,z), site(x, y,z)] = -1.0 
+          K[site(x, y,z), site(xp,y,z)] = -1.0
+        if (yp > y) or pery:
+          K[site(x,yp,z), site(x,y, z)] = -1.0 
+          K[site(x,y, z), site(x,yp,z)] = -1.0
+        if (zp > z) or perz:
+          K[site(x,y,zp), site(x,y,z )] = -1.0
+          K[site(x,y,z ), site(x,y,zp)] = -1.0
+  
+  Eval,Evec = np.linalg.eigh(K) #, b=None, left=False, right=True, overwrite_a=False, overwrite_b=False, check_finite=True)
+  print Evec
+  P = 1./2. * np.matrix(Evec) * np.matrix(np.diag(np.sqrt(Eval))) * np.matrix(Evec.T)
+  X = 1./2. * np.matrix(Evec) * np.matrix(np.diag(1. / np.sqrt(Eval))) * np.matrix(Evec.T)
+  
+#   filename=file.split(".")[0]
+#   db = dbm.open(filename,'w')
+#   for key in db.keys():
+#       if db[key] == str(-99):
+#           clust = np.array(kg.decompress(key))
+#           #print clust
+#           sitesA = []
+#           for y in range(0, Ly):
+#               for x in range(0, Lx):
+#                   if clust[y, x] == 1:
+#                       sitesA.append(site(x, y))
+#           NsA=len(sitesA)
+#           Pred = np.zeros((NsA, NsA))
+#           Xred = np.zeros((NsA, NsA))
+#           # ............ Following step differs from Mathematica ...........
+#           #Get X and P for sites inside A only:
+#           for a in range(0, NsA):
+#               for b in range(0, NsA):
+#                   Pred[a,b] = P[sitesA[a] , sitesA[b]]
+#                   Xred[a,b] = X[sitesA[a] , sitesA[b]]
+#                   #....end of the step .......
+# 
+#           Csquared = (Xred.T).dot(Pred)
+#           Ev = np.sqrt(np.linalg.eigvals(Csquared))
+#           Sn = 0. 
+#           for j in range(0, NsA):
+#               if Ev[j] > 0.5:
+#                   Sn += (Ev[j]+1./2) * np.log(abs(Ev[j]+1./2.))-(Ev[j]-1./2.)*np.log(abs(Ev[j]-1./2)) 
+#           db[key] = str("%.15f"%Sn)
+#   db.close()
+# 
+#   newfilename = filename.split("/")[0]+"/"+filename.split("/")[1][2:]
+#   os.rename(filename+".db", newfilename+".db")
+#   print file , "done"
+#     
+#   print "Free boson calculation complete" 
---
+++ mxn_weight.py
@@ -0,0 +1,33 @@
+import numpy
+import scipy as sp
+#import property_gen as pg
+
+def weight(m,n,w):
+  w_mxn_name = '%03d%03d'%(m,n)
+  
+  # First term in weight of mxn is property of mxn
+  ### !!! WRITE CODE TO CALCULATE PROPERTY:
+  ### w[w_mxn_name] = pg.property(m,n,Lx,Ly,lattice)
+  w[w_mxn_name] = 0
+  
+  wformula = "W%03d%03d=P%03d%03d"%(m,n,m,n)
+  print wformula
+  print
+  
+  for y in range (1,n+1):
+    for x in range (y,m+1):
+      if (y < n or x < m) and x > 1:
+        print wformula
+        
+        if x > n or x==y: coeff = (m-x+1)*(n-y+1)
+        else: coeff = (m-x+1)*(n-y+1)+(m-y+1)*(n-x+1)
+        
+        wformula += "%+d*W%03d%03d"%(-coeff,x,y)
+        
+        ### !!! Write NLCE base case# 
+        ### w[w_mxn_name] -= coeff * w['%03d%03d'%(x,y)]
+        
+  ###print w[w_mxn_name]
+  print wformula
+  
+  return w
