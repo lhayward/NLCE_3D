c1cdb396f185640e685b1306c48cf25dbace2006
lhayward <l3haywar@uwaterloo.ca>
2015-07-07 20:10:20 -0400

    Updated corner entropy calculation

--- free_boson_3D.py
+++ free_boson_3D.py
@@ -14,69 +14,89 @@ def getCornerEnt(Lx,Ly,Lz):
 
   Ns = Lx * Ly * Lz
 
-  def site(x, y, z):
-    return x + (y*Lx) + (z*Lx*Ly) #convert (x,y,z) pair to a single site number
-
   K = np.zeros((Ns, Ns))
   #Loop to calculate the matrix K:
   for x in range(Lx):
     for y in range(Ly):
       for z in range(Lz):
-        K[site(x,y,z),site(x,y,z)]= 6.0 + (massterm ** (2))
+        K[site(Lx,Ly,Lz,x,y,z),site(Lx,Ly,Lz,x,y,z)]= 6.0 + (massterm ** (2))
         xp = (x+1)%Lx
         yp = (y+1)%Ly
         zp = (z+1)%Lz
         if (xp > x) or perx:
-          K[site(xp,y,z), site(x, y,z)] = -1.0 
-          K[site(x, y,z), site(xp,y,z)] = -1.0
+          K[site(Lx,Ly,Lz,xp,y,z), site(Lx,Ly,Lz,x, y,z)] = -1.0 
+          K[site(Lx,Ly,Lz,x, y,z), site(Lx,Ly,Lz,xp,y,z)] = -1.0
         if (yp > y) or pery:
-          K[site(x,yp,z), site(x,y, z)] = -1.0 
-          K[site(x,y, z), site(x,yp,z)] = -1.0
+          K[site(Lx,Ly,Lz,x,yp,z), site(Lx,Ly,Lz,x,y, z)] = -1.0 
+          K[site(Lx,Ly,Lz,x,y, z), site(Lx,Ly,Lz,x,yp,z)] = -1.0
         if (zp > z) or perz:
-          K[site(x,y,zp), site(x,y,z )] = -1.0
-          K[site(x,y,z ), site(x,y,zp)] = -1.0
+          K[site(Lx,Ly,Lz,x,y,zp), site(Lx,Ly,Lz,x,y,z )] = -1.0
+          K[site(Lx,Ly,Lz,x,y,z ), site(Lx,Ly,Lz,x,y,zp)] = -1.0
   
   Eval,Evec = np.linalg.eigh(K) #, b=None, left=False, right=True, overwrite_a=False, overwrite_b=False, check_finite=True)
-  print Evec
+  #print Evec
   P = 1./2. * np.matrix(Evec) * np.matrix(np.diag(np.sqrt(Eval))) * np.matrix(Evec.T)
   X = 1./2. * np.matrix(Evec) * np.matrix(np.diag(1. / np.sqrt(Eval))) * np.matrix(Evec.T)
   
-#   filename=file.split(".")[0]
-#   db = dbm.open(filename,'w')
-#   for key in db.keys():
-#       if db[key] == str(-99):
-#           clust = np.array(kg.decompress(key))
-#           #print clust
-#           sitesA = []
-#           for y in range(0, Ly):
-#               for x in range(0, Lx):
-#                   if clust[y, x] == 1:
-#                       sitesA.append(site(x, y))
-#           NsA=len(sitesA)
-#           Pred = np.zeros((NsA, NsA))
-#           Xred = np.zeros((NsA, NsA))
-#           # ............ Following step differs from Mathematica ...........
-#           #Get X and P for sites inside A only:
-#           for a in range(0, NsA):
-#               for b in range(0, NsA):
-#                   Pred[a,b] = P[sitesA[a] , sitesA[b]]
-#                   Xred[a,b] = X[sitesA[a] , sitesA[b]]
-#                   #....end of the step .......
-# 
-#           Csquared = (Xred.T).dot(Pred)
-#           Ev = np.sqrt(np.linalg.eigvals(Csquared))
-#           Sn = 0. 
-#           for j in range(0, NsA):
-#               if Ev[j] > 0.5:
-#                   Sn += (Ev[j]+1./2) * np.log(abs(Ev[j]+1./2.))-(Ev[j]-1./2.)*np.log(abs(Ev[j]-1./2)) 
-#           db[key] = str("%.15f"%Sn)
-#   db.close()
-# 
-#   newfilename = filename.split("/")[0]+"/"+filename.split("/")[1][2:]
-#   os.rename(filename+".db", newfilename+".db")
-#   print file , "done"
-#     
-#   print "Free boson calculation complete"
+  result=0
+  for x0 in range(1,Lx):
+    for y0 in range(1,Ly):
+      for z0 in range(1,Lz):
+        #corners:
+        result += 1.0/4.0*(getEntropy(Lx,Ly,Lz,getRegionA(Lx,Ly,Lz,x0,y0,z0,cmp.geq,cmp.geq,cmp.geq),X,P) 
+                         + getEntropy(Lx,Ly,Lz,getRegionA(Lx,Ly,Lz,x0,y0,z0,cmp.lt, cmp.lt, cmp.geq),X,P) 
+                         + getEntropy(Lx,Ly,Lz,getRegionA(Lx,Ly,Lz,x0,y0,z0,cmp.geq,cmp.lt, cmp.lt ),X,P)
+                         + getEntropy(Lx,Ly,Lz,getRegionA(Lx,Ly,Lz,x0,y0,z0,cmp.lt, cmp.geq,cmp.lt ),X,P))
+        #edges:
+        result += 1.0/4.0*(getEntropy(Lx,Ly,Lz,getRegionA(Lx,Ly,Lz,x0,y0,z0,cmp.geq,cmp.geq,cmp.any),X,P) 
+                         + getEntropy(Lx,Ly,Lz,getRegionA(Lx,Ly,Lz,x0,y0,z0,cmp.lt, cmp.lt ,cmp.any),X,P) 
+                         + getEntropy(Lx,Ly,Lz,getRegionA(Lx,Ly,Lz,x0,y0,z0,cmp.geq,cmp.any,cmp.geq),X,P)
+                         + getEntropy(Lx,Ly,Lz,getRegionA(Lx,Ly,Lz,x0,y0,z0,cmp.lt ,cmp.any,cmp.lt ),X,P)
+                         + getEntropy(Lx,Ly,Lz,getRegionA(Lx,Ly,Lz,x0,y0,z0,cmp.any,cmp.geq,cmp.geq),X,P)
+                         + getEntropy(Lx,Ly,Lz,getRegionA(Lx,Ly,Lz,x0,y0,z0,cmp.any,cmp.lt ,cmp.lt ),X,P))
+        #planes:
+        result += 1.0/4.0*(getEntropy(Lx,Ly,Lz,getRegionA(Lx,Ly,Lz,x0,y0,z0,cmp.geq,cmp.any,cmp.any),X,P) 
+                         + getEntropy(Lx,Ly,Lz,getRegionA(Lx,Ly,Lz,x0,y0,z0,cmp.any,cmp.geq,cmp.any),X,P) 
+                         + getEntropy(Lx,Ly,Lz,getRegionA(Lx,Ly,Lz,x0,y0,z0,cmp.any,cmp.any,cmp.geq),X,P))
+      #loop over z0
+    #loop over y0
+  #loop over x0
+  return result
+#.......................................END getCornerEnt.......................................
+
+def getEntropy(Lx,Ly,Lz,regA,X,P):
+  sitesA = []
+  for x in range(Lx):
+    for y in range(Ly):
+      for z in range(Lz):
+        if regA[x,y,z] == True: sitesA.append(site(Lx,Ly,Lz,x,y,z))
+  NsA=len(sitesA)
+  Pred = np.zeros((NsA, NsA))
+  Xred = np.zeros((NsA, NsA))
+
+  # ............ Following step differs from Mathematica ...........
+  #Get X and P for sites inside A only:
+  for a in range(0, NsA):
+    for b in range(0, NsA):
+        Pred[a,b] = P[sitesA[a] , sitesA[b]]
+        Xred[a,b] = X[sitesA[a] , sitesA[b]]
+  #....end of the step .......
+
+  Csquared = (Xred.T).dot(Pred)
+  Ev = np.sqrt(np.linalg.eigvals(Csquared))
+  #print np.linalg.eigvals(Csquared)
+  #print
+  Sn = 0. 
+  for j in range(0, NsA):
+    if Ev[j] > 0.5:
+      Sn += (Ev[j]+1./2) * np.log(abs(Ev[j]+1./2.))-(Ev[j]-1./2.)*np.log(abs(Ev[j]-1./2)) 
+  if (np.abs(Sn-0.574433)<0.00001):
+    print Sn
+    print Csquared
+    print np.linalg.eigvals(Csquared)
+    print
+  return Sn
+#........................................END getEntropy........................................
 
 def getRegionA(Lx,Ly,Lz,x0,y0,z0,fx,fy,fz):
   regA = np.zeros( (Lx,Ly,Lz), dtype='bool' )
@@ -95,3 +115,6 @@ def getRegionA(Lx,Ly,Lz,x0,y0,z0,fx,fy,fz):
   if( (regA==True).sum() > (Lx*Ly*Lz/2) ): regA = np.logical_not(regA)
   
   return regA
+  
+def site(Lx,Ly,Lz,x,y,z):
+    return x + (y*Lx) + (z*Lx*Ly) #convert (x,y,z) pair to a single site number
