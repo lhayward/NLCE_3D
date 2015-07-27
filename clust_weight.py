fe6509a85c29e9892b47bc0db673327135f041af
lhayward <l3haywar@uwaterloo.ca>
2015-07-27 17:20:27 -0400

    Added in if statement (in clust_weight.py) to check if w[w_clust_name] has already been calculated

--- clust_weight.py
+++ clust_weight.py
@@ -6,30 +6,32 @@ import free_boson_3D
 def weight(Lx,Ly,Lz,w,alpha,massterm):
   w_clust_name = clust_name(Lx,Ly,Lz)
   
-  # First term in weight of this cluster is the property of the cluster:
-  w[w_clust_name] = free_boson_3D.getCornerEnt(Lx,Ly,Lz,alpha,massterm)
+  if w_clust_name not in w:
+    #First term in weight of this cluster is the property of the cluster:
+    w[w_clust_name] = free_boson_3D.getCornerEnt(Lx,Ly,Lz,alpha,massterm)
   
-  wformula = "W(" + clust_name(Lx,Ly,Lz) + ") = P(" + clust_name(Lx,Ly,Lz) + ")"
-  #print wformula
+    wformula = "W(" + clust_name(Lx,Ly,Lz) + ") = P(" + clust_name(Lx,Ly,Lz) + ")"
+    #print wformula
   
-  # Subtract weights of all subclusters:
-  for x in range(1, Lx+1):
-    for y in range(1, Ly+1):
-      for z in range(1, Lz+1):
-        # Check that we are not at the Lx x Ly x Lz cluster, and also check that we are not at
-        # the 1 x 1 x 1 cluster (both of these cases don't contribute:
-        if (x!=Lx or y!=Ly or z!=Lz) and max(x,y,z)>1:
+    # Subtract weights of all subclusters:
+    for x in range(1, Lx+1):
+      for y in range(1, Ly+1):
+        for z in range(1, Lz+1):
+          # Check that we are not at the Lx x Ly x Lz cluster, and also check that we are not at
+          # the 1 x 1 x 1 cluster (both of these cases don't contribute:
+          if (x!=Lx or y!=Ly or z!=Lz) and max(x,y,z)>1:
         
-          coeff = (Lx-x+1)*(Ly-y+1)*(Lz-z+1)
+            coeff = (Lx-x+1)*(Ly-y+1)*(Lz-z+1)
           
-          #weight is stored such that x<=y<=z, so sort the current x,y,z
-          [xs,ys,zs] = sorted([x,y,z])
+            #weight is stored such that x<=y<=z, so sort the current x,y,z
+            [xs,ys,zs] = sorted([x,y,z])
           
-          wformula += "+ %d*W("%(-coeff) + clust_name(xs,ys,zs) + ")"
-          w[w_clust_name] -= coeff * w[clust_name(xs,ys,zs)]
+            wformula += "+ %d*W("%(-coeff) + clust_name(xs,ys,zs) + ")"
+            w[w_clust_name] -= coeff * w[clust_name(xs,ys,zs)]
           
-  #print wformula
-  #print
+    #print wformula
+    #print
+  #end if
   
   return w
   
